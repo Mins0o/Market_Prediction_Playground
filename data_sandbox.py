56c79b1ffeed0ecd1f116a66949a277489014e1d
MINSOO KANG <stengine2@gmail.com>
2023-10-02 00:12:50 +0900

    Training with custom activation

--- data_sandbox.py
+++ data_sandbox.py
@@ -1,1 +1,20 @@
-from pykrx import stock
+import pandas as pd
+import pickle
+
+
+def load_data():
+    with open("KRX_data.pkl", "rb") as data_file:
+        all_data = pickle.load(data_file)
+    return all_data
+
+def combine_single_column(all_data, column_name):
+    columns_collected = [df["data"][column_name] for df in all_data]
+    for ii, column in enumerate(columns_collected):
+        column.name=all_data[ii]["name"]
+    combined = pd.concat(columns_collected, axis=1)
+    return combined
+
+def save_combined_data(all_data, column_name):
+    combined = combine_single_column(all_data, column_name)
+    with open(f"{column_name}.pkl", "wb") as save_file:
+        pickle.dump(combined, save_file)
--- training_sandbox.py
+++ training_sandbox.py
@@ -5,84 +5,66 @@ import matplotlib.pyplot as plt
 import pickle
 import datetime
 
-INPUT_WINDOW_SIZE = 5
-BATCH_SIZE = 32
 CHECKPOINT_DIR = './training_checkpoints'
 
 def load_data():
-    with open("test.pkl", "rb") as f:
+    with open("Change.pkl", "rb") as f:
         data = pickle.load(f)
+    data=data.fillna(0)
+    return data
 
-    rename_column = lambda df: df[1][["Close"]].rename(
-                                columns={"Close":df[0]["종목명"]})
-
-    combined = pd.concat([rename_column(ii) for ii in data], axis=1)
-    combined = combined.fillna(0).astype(int)
-
-    np_comb = np.array(combined)
-    return np_comb
-
-def create_binary_outputs(batch):
-    last_two = batch[:, -2:, :]
-    increased = tf.math.greater(last_two[:, 1, :], last_two[:,0,:])
-    binary_output = tf.cast(increased, dtype=tf.float32)
-    return batch[:, :-1, :], binary_output
-
-def prepare_dataset(dataset_base):
-    dataset = tf.keras.utils.timeseries_dataset_from_array(
-        data = dataset_base,
-        targets = None,
-        sequence_length = INPUT_WINDOW_SIZE+1,
-        sequence_stride = 1,
-        shuffle = True,
-        batch_size = BATCH_SIZE,
-    )
-    dataset = dataset.map(create_binary_outputs)
-    return dataset
-
-def create_dataset(np_data_base):
-    train_ds_base = np_data_base[:7000]
-    test_ds_base = np_data_base[7000:]
-    ds_train = prepare_dataset(train_ds_base).take(220)
-    ds_test = prepare_dataset(test_ds_base).take(30)
-    return ds_train, ds_test
 
 def train_model(model, loss_fn, metrics, callbacks):
-    np_data_base = load_data()
-    ds_train, ds_test = create_dataset(np_data_base)
+    np_data_base = np.array(load_data())
+    ds_train = np_data_base[:-1].reshape((-1,1,3446))
+    ds_test = np_data_base[1:]
 
     model.compile(
-        optimizer = tf.keras.optimizers.Adam(0.001),
+        optimizer = tf.keras.optimizers.Adam(0.01),
         loss = loss_fn,
         metrics = metrics
     )
 
     model.fit(
         ds_train,
-        epochs = 30,
-        validation_data = ds_test,
+        ds_test,
+        epochs = 1000,
+        batch_size = 1000,
+        validation_split = 0.1,
         callbacks=callbacks
     )
 
+def create_custom_activation():
+    def activation(x):
+        eps = 0.1
+        slope = 0.05 
+        logit = lambda x: tf.math.log(x / (1 - x + eps))
+        push_aside = logit(eps/(30+eps))
+        positive_new_sigmoid = (30+eps) * tf.sigmoid(x*slope + push_aside) - eps
+        negative_new_sigmoid = -(30+eps) * tf.sigmoid(-x*slope + push_aside) + eps
+        return tf.where(x > 0, positive_new_sigmoid, negative_new_sigmoid)
+    return activation
+
+
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(INPUT_WINDOW_SIZE,2754)),
-    tf.keras.layers.Dense(2754*INPUT_WINDOW_SIZE, activation = "relu"),
-    tf.keras.layers.Dense(10_000, activation = "relu"),
-    tf.keras.layers.Dense(8000, activation = "relu"),
-    tf.keras.layers.Dense(2754, activation = "sigmoid"),
+    tf.keras.layers.LSTM(500, return_sequences=True, input_shape=(1, 3446)),
+    tf.keras.layers.LSTM(500),
+    tf.keras.layers.Dense(3446, activation = create_custom_activation())
 ])
 
-loss_fn = tf.keras.losses.BinaryCrossentropy()
-metrics = [tf.keras.metrics.BinaryAccuracy()]
+loss_fn = tf.keras.losses.MeanSquaredError()
+metrics = [tf.keras.metrics.MeanAbsoluteError()]
 
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 callbacks = [tf.keras.callbacks.ModelCheckpoint(
                 filepath=CHECKPOINT_DIR + "/ckpt", 
                 save_best_only=True, 
-                monitor='val_binary_accuracy', 
                 verbose=1, 
                 mode='auto', 
                 save_freq='epoch'
             ),
             tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 ]
+
+if __name__ == "__main__":
+    train_model(model, loss_fn, metrics, callbacks)
