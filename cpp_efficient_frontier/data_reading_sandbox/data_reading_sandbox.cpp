b189d6bc7f2239a8f9cec6a61f93b61de5178ba6
Mins0o <memesoo.kang@gmail.com>
2023-10-26 15:25:13 +0900

    Memo on "package seeking" for a feature

--- cpp_efficient_frontier/data_reading_sandbox/data_reading_sandbox.cpp
+++ cpp_efficient_frontier/data_reading_sandbox/data_reading_sandbox.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+/* I've done some research on how to "read csv" files in cpp.
+With my background coming from Python,
+it seems like there should be a easy to use package that
+can handle various formats and datatypes.
+(I also had to learn what a package manager equivalent of writing c/cxx code is)
+
+My conclusion for now is, there are libraries 
+that facilitate reading the file and handling the strings,
+but I am on my own to write a data structure and methods of parsing.
+*/
+
 
 #include "data_reading_sandbox.hpp"
 
