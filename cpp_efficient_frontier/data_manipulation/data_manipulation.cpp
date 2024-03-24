69af671890dc5ff44a5c8a56c9734114c9021c7d
Minsoo Kang <memesoo.kang@gmail.com>
2024-03-25 02:25:42 +0900

    parsing data from ifstream - 01
    retreiving symbol names and reserving return table.

--- cpp_efficient_frontier/data_manipulation/data_manipulation.cpp
+++ cpp_efficient_frontier/data_manipulation/data_manipulation.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
 #include "data_manipulation.hpp"
 
 void test_data_manipulation(){
@@ -6,8 +10,46 @@ void test_data_manipulation(){
 
 namespace data{
 
+/**
+ * Takes in the file stream and parses it
+*/
 data::Data::Data(std::ifstream& data_file_stream){
     std::cout << "file stream constructor" << std::endl;
+    parse_(data_file_stream);
+}
+
+Tokens& Data::tokenize_line_(/*I*/ const std::string line, 
+                             /*O*/ Tokens& tokens_return) const{
+    std::stringstream line_stream(line);
+    std::string token;
+    while (std::getline(line_stream, token, '\t')){
+        tokens_return.push_back(token);
+    }
+    return tokens_return;
+}
+
+Tokens& Data::parse_first_line_(/*I*/ const std::string first_line,
+                                /*O*/ Tokens& symbol_names) const{
+    symbol_names = tokenize_line_(first_line, symbol_names);
+    symbol_names.erase(symbol_names.begin());
+    return symbol_names;
+}
+
+void Data::parse_(std::ifstream& parsing_stream){
+    parsing_stream.clear();
+    parsing_stream.seekg(0);
+
+    std::string first_line;
+
+    std::getline(parsing_stream, first_line);
+    symbol_names_ = parse_first_line_(first_line, symbol_names_);
+    column_count_ = symbol_names_.size();
+
+    return_table_.reserve(column_count_);
+    for(int ii=0; ii<column_count_; ii++){
+        return_table_.emplace_back();
+        return_table_.back().reserve(9000);
+    }
 }
 
 }
--- cpp_efficient_frontier/data_manipulation/data_manipulation.hpp
+++ cpp_efficient_frontier/data_manipulation/data_manipulation.hpp
@@ -1,14 +1,50 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <map>
+#include <ctime>
 
 void test_data_manipulation();
+
+typedef std::map<std::time_t,size_t> DateMap;
+typedef std::map<std::string,size_t> NameMap;
+typedef std::vector<std::vector<double>> FloatArray;
+typedef std::vector<std::string> Tokens;
 
 namespace data{
 class Data{
 private:
     size_t row_count_ = 0;
     size_t column_count_ = 0;
+    DateMap date_map_;
+    NameMap name_map_;
+    FloatArray return_table_;
+    Tokens symbol_names_;
 public:
+    int dummy;
+private:
+    Tokens& tokenize_line_(/*I*/ const std::string line,
+                           /*O*/ Tokens& tokens_return) const;
+    /**
+     * Takes the first line (:string)) of the file to get the table headers
+     * which is the symbols of the security.
+     * @param first_line <string> first line of the data table file
+     * @return tokenized and trimmed symbols of securities.
+    */
+    Tokens& parse_first_line_(/*In*/ const std::string first_line,
+                              /*Out*/ Tokens& tokens_return) const;
+    void parse_(std::ifstream& parsing_stream);
+public:
+    /**
+     * Constructor with std::ifstream.
+     * Parses data from the file stream and stores in to this instance.
+     * 
+     * @param parsing_stream currently being developed for `.tsv` format
+    */
     Data(std::ifstream& data_file_stream);
+
+    void trim();
+    void select_date_range();
+    void select_stock();
 };
 }
