f6402854733bf763cfd2bd2575b814293c1101b5
Minsoo <memesoo.kang@gmail.com>
2024-04-17 00:38:43 +0900

    Calculate the changes in ETFs

--- python_data_collection/data_sandbox.py
+++ python_data_collection/data_sandbox.py
@@ -20,7 +20,18 @@ def save_combined_data(all_data, column_name, dir_name="data_pkl"):
     with open(os.path.join(dir_name,column_name)+".pkl", "wb") as save_file:
         pickle.dump(combined, save_file)
 
-def save_combined_data_as_csv(all_data, column_name, dir_name="data_pkl", delimitter=","):
+def save_combined_data_as_csv(all_data, column_name, dir_name="data_csv", delimitter=","):
     combined = combine_single_column(all_data, column_name)
     csv_filename = os.path.join(dir_name,f'{column_name}.csv')
-    combined.to_csv(csv_filename, index=False, sep = delimitter)
+    combined.to_csv(csv_filename, sep = delimitter)
+
+def process_etf_data(etf_all_data):
+    for entry in etf_all_data:
+        change=[]
+        prev=1
+        for close in entry["data"]["Close"]:
+            change.append(((close/prev)-1)*100)
+            prev = close
+        change[0] = np.NaN
+        entry["data"]["Change"] = change
+    return etf_all_data
