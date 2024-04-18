16dbd2fb3329a327ab64f018824325f3fb72f90e
Minsoo <memesoo.kang@gmail.com>
2024-04-18 22:03:07 +0900

    clustering the test to do 2 security at a time

--- cpp_efficient_frontier/program.cpp
+++ cpp_efficient_frontier/program.cpp
@@ -10,19 +10,43 @@ void read_file(const char* file_path, std::ifstream& read_file){
     read_file = std::ifstream(file_path);
 }
 
+std::vector<double> test_suite(const size_t stock_number, data::Data& testing_data){
+    std::string symbol_name = testing_data.get_symbol_name(stock_number);
+    auto start_date = testing_data.get_start_date(stock_number);
+    auto end_date = testing_data.get_end_date(stock_number);
+    std::cout << stock_number << " " << symbol_name << 
+                "\nstart_date: " << ctime(&start_date) <<
+                "end_date: " << ctime(&end_date);
+    std::cin.get();
+
+    std::vector<double> symbol_values = testing_data.select_symbol(stock_number);
+
+    auto trimmed = testing_data.trim(symbol_values, start_date, end_date);
+
+    std::vector<double> net_returns1;
+    double net_return = calculations::Calculations::net_return(trimmed);
+    calculations::Calculations::value_series(trimmed, net_returns1);
+    double average = calculations::Calculations::average(trimmed);
+    double stdv = calculations::Calculations::standard_deviation(trimmed);
+
+    std::cout << trimmed.front() << " "
+                << trimmed.back() << " " 
+                << trimmed.size() << std::endl
+                << symbol_name << std::endl
+                << net_return << std::endl
+                << "avg " << average << std::endl
+                << "stdv " << stdv << std::endl;
+
+    for(int ii = 0; ii < trimmed.size()/1'000; ii++){
+        std::cout << trimmed[ii] << " " << net_returns1[ii] << std::endl;
+        std::cin.get();
+    }
+    return(symbol_values);
+}
+
 int main(int argc, char* argv[]){
     /*
-    select data file
-    read data file (as text I guess)
-    
-    ⬇ What should be passed into parsing?
-    give ifstream for now.
-
     put the data into a easier-to-handle format (Parse)
-     - get single stock record
-      - get selected stock records
-     - get date snapshot
-      - get range of date snapsot
      - cropping(?) data horizontally (columns) and vertically (dates)
      - browse stocks (read names and select)
       - read stock names
@@ -36,42 +60,26 @@ int main(int argc, char* argv[]){
 
     std::srand(std::time(0));
 
+    std::cout << testing_data.get_symbols_count() << std::endl;
+    
+    tm temporary_tm = {0};
+    strptime("2024-03-01", "%Y-%m-%d", &temporary_tm);
+    std::time_t start_time = mktime(&temporary_tm);
+    strptime("2024-04-01", "%Y-%m-%d", &temporary_tm);
+    std::time_t end_time = mktime(&temporary_tm);
+
+
     for(int ii=0; ii<30; ii++){
-        int stock_lookup = std::rand() % 3526;
-        std::string symbol_name = testing_data.get_symbol_name(stock_lookup);
-        auto start_date = testing_data.get_start_date(stock_lookup);
-        auto end_date = testing_data.get_end_date(stock_lookup);
-        std::cout << stock_lookup << " " << symbol_name << 
-                    "\nstart_date: " << ctime(&start_date) <<
-                    "\nend_date: " << ctime(&end_date);
-        std::cin.get();
+        int stock_number1 = std::rand() % testing_data.get_symbols_count();
+        auto values1 = test_suite(stock_number1, testing_data);
+        auto trimmed01 = testing_data.trim(values1, start_time,end_time);
+        int stock_number2 = std::rand() % testing_data.get_symbols_count();
+        auto values2 = test_suite(stock_number2, testing_data);
+        auto trimmed02 = testing_data.trim(values2, start_time,end_time);
+        auto summed = calculations::Calculations::weighted_sum(trimmed01,0.3,trimmed02,0.7);
 
-        std::vector<double> symbol_values = testing_data.select_symbol(stock_lookup);
-
-        for(int jj=0; jj<testing_data.time_point_count(); jj++){
-            auto temp_t = testing_data.get_date(jj);
-            std::cout << symbol_values[jj] << /*std::endl <<*/ ctime(&temp_t);
-        }
-        std::cin.get();
-
-        auto trimmed = testing_data.trim(symbol_values, start_date, end_date);
-        std::vector<double> net_returns;
-        double net_return = calculations::Calculations::net_return(trimmed);
-        calculations::Calculations::value_series(trimmed, net_returns);
-        double average = calculations::Calculations::average(trimmed);
-        double stdv = calculations::Calculations::standard_deviation(trimmed);
-
-        std::cout << trimmed.front() << " "
-                  << trimmed.back() << " " 
-                  << trimmed.size() << std::endl
-                  << symbol_name << std::endl
-                  << net_return << std::endl
-                  << "avg " << average << std::endl
-                  << "stdv " << stdv << std::endl;
-
-        for(int ii = 0; ii < trimmed.size()/1'000; ii++){
-            std::cout << trimmed[ii] << " " << net_returns[ii] << std::endl;
-            std::cin.get();
+        for (size_t ii=0; ii < summed.size(); ii++){
+            std::cout << trimmed01[ii] << "\t" << trimmed02[ii] << "\t" << summed[ii] << std::endl;
         }
     }
 }
