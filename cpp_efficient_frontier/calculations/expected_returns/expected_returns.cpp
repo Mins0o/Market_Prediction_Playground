ca6a8a14f788efac8c283e8a1ea973214fec3d26
Minsoo <memesoo.kang@gmail.com>
2024-05-16 22:46:47 +0900

    separating test with main

--- cpp_efficient_frontier/calculations/expected_returns/expected_returns.cpp
+++ cpp_efficient_frontier/calculations/expected_returns/expected_returns.cpp
@@ -1,5 +1,6 @@
 #include <numeric>
 #include <vector>
+#include <cmath>
 #include "expected_returns.hpp"
 
 double ArithmeticMeanStrategy::get_expected_return(/*I*/ const std::vector<double>& returns_list){
@@ -9,9 +10,35 @@ double ArithmeticMeanStrategy::get_expected_return(/*I*/ const std::vector<doubl
      return std::reduce(returns_list.begin(), returns_list.end())/returns_list.size();
 }
 
-double StatisticalStrategy::get_expected_return(const std::vector<double>& returns_list){
+DiscountedMeanStrategy::DiscountedMeanStrategy(){
+     discount_rate_ = 0;
+}
+
+/**
+ * @discount_rate bigger rate ignores the past values more strongly. value x means the last element is weighted 10^-x
+*/
+DiscountedMeanStrategy::DiscountedMeanStrategy(double dicount_rate){
+     discount_rate_ = discount_rate_;
+}
+
+double DiscountedMeanStrategy::get_expected_return(const std::vector<double>& returns_list){
      if (returns_list.empty()){
           return 0.0;
      }
-     return 0.0;
+     if (returns_list.size()==1){
+          return returns_list[0];
+     }
+
+     size_t array_size = returns_list.size();
+     double weight_sum = 0;
+     double weighted_sum = 0;
+
+     for (int ii = 0; ii<array_size; ii++){
+          double power_value = -(ii+0.0)/(array_size-1);
+          double weight_value = std::pow(10, power_value * discount_rate_);
+          weighted_sum += returns_list[array_size-ii-1]*weight_value;
+          weight_sum += weight_value;
+     }
+
+     return weighted_sum/weight_sum;
 }
--- cpp_efficient_frontier/calculations/expected_returns/expected_returns.hpp
+++ cpp_efficient_frontier/calculations/expected_returns/expected_returns.hpp
@@ -8,7 +8,12 @@ public:
     double get_expected_return(const std::vector<double>& returns_list);
 };
 
-class StatisticalStrategy: public ExpectedReturnStrategy{
+class DiscountedMeanStrategy: public ExpectedReturnStrategy{
+private:
+    double discount_rate_ = 0;
 public:
+    DiscountedMeanStrategy();
+    DiscountedMeanStrategy(double decay_rate);
     double get_expected_return(const std::vector<double>& returns_list);
 };
+
