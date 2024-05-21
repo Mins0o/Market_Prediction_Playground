7b2f284b48adb2bbc11ef797d91b7653a430fc21
Minsoo <memesoo.kang@gmail.com>
2024-05-21 22:32:20 +0900

    Cleanup pseudo code

--- cpp_efficient_frontier/program.cpp
+++ cpp_efficient_frontier/program.cpp
@@ -25,20 +25,9 @@ int main(int argc, char* argv[]){
 	portfolio_data optimal_mixes[3] = {};
 	optimize_portfolio(processed, optimal_mixes);
 	
-	
-	// read file
-	// choose securities
-	// choose investment term
-	// get weights for maximum sharpe ratio, with the returns and risks
-		// with the securities
-		// for n times
-			// make random weight mix
-			// compound by terms
-			// get expected return
-			// get risk
-			// get sharpe ratio
-			// make a data with sharpe, expected return, risk, and weight
-			// put the calculation result in an array
-	// sort the array (by sharpe, or by expected return, or by risk)
-	// get the desired outcome
+	/**
+	 *TODO
+	 * choose investment term
+	 * compound by terms
+	*/
 }
