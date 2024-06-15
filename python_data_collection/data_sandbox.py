add428220a116b1f1047ae295e39d31f52f6a0a0
Minsoo <memesoo.kang@gmail.com>
2024-06-15 18:09:52 +0900

    Security diff function
    - Implement `find_added_and_deleted_data` to identify newly added and removed securities by comparing their names between two datasets.
    - Add `index_data` function to create indices for securities based on their names, facilitating faster searches.
    - Add `search_security` function that utilizes the indices from `index_data` for an optimized search process.

--- python_data_collection/data_sandbox.py
+++ python_data_collection/data_sandbox.py
@@ -36,3 +36,45 @@ def process_etf_data(etf_all_data):
         change[0] = np.NaN
         entry["data"]["Change"] = change
     return etf_all_data
+
+def find_added_and_deleted_data(old_data, new_data):
+    """
+    Find the data that were added and deleted by comparing the names of the data.
+    Each data is a list of dictionaries with keys "symbol", "name", and "data".
+    """
+    old_names = set([entry["name"] for entry in old_data])
+    new_names = set([entry["name"] for entry in new_data])
+    added = new_names - old_names
+    deleted = old_names - new_names
+    return added, deleted
+
+def index_data(data):
+    """
+    Index the data by the names of the securities.
+    The return indices will be used in the `search_security` function.
+    Args:
+        data (list): A list of dictionaries containing the symbol, name, and data for each security.
+    Returns:
+        indices (dict): A dictionary with the names of the securities as keys and the corresponding index as values.
+    """
+    indices = {}
+    for i, entry in enumerate(data):
+        indices[entry["name"]] = i
+    return indices
+
+def search_security(data, indices, name):
+    """
+    Search for the data of a security by its name.
+    Optimized by using the indices obtained from the `index_data` function.
+    Args:
+        data (list): A list of dictionaries containing the symbol, name, and data for each security.
+        indices (dict): A dictionary with the names of the securities as keys and the corresponding index as values.
+        name (str): The name of the security to search for.
+    Returns:
+        security_data (dict): A dictionary containing the symbol, name, and data for the specified security.
+    """
+    index = indices.get(name)
+    if index is not None:
+        return data[index]
+    else:
+        return None
