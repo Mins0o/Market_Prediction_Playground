f77178780b60202ed05f320c64fb4c197265140a
Minsoo <memesoo.kang@gmail.com>
2024-06-15 18:01:29 +0900

    ticker --> security, documentaions

--- python_data_collection/data_scraping_sandbox.py
+++ python_data_collection/data_scraping_sandbox.py
@@ -18,7 +18,7 @@ OUTPUT_LOCK=threading.Lock()
 class OhlcvThread(threading.Thread):
     def __init__(self, target_info):
         threading.Thread.__init__(self)
-        self.ticker = target_info["ticker"]
+        self.security = target_info["security"]
         self.fetcherf = target_info["fetcherf"]
         self.result = None
     
@@ -26,12 +26,12 @@ class OhlcvThread(threading.Thread):
         fetched = pd.DataFrame()
         trial_count=0
         while fetched.empty and trial_count<=100:
-            fetched = self.fetcherf(self.ticker)
+            fetched = self.fetcherf(self.security)
             trial_count += 1
             time.sleep(60)
         if trial_count > 100:
             OUTPUT_LOCK.acquire()
-            print(f"\n{self.ticker} failed")
+            print(f"\n{self.security} failed")
             OUTPUT_LOCK.release()
         fetched = fetched.rename(columns=OHLCV_REPLACE_NAMINGS)
         fetched.index.name = "data"
@@ -39,12 +39,12 @@ class OhlcvThread(threading.Thread):
 
 class DataFetcher():
     def __init__(self):
-        self._ticker_fetcherf = None
+        self._security_fetcherf = None
         self._name_fetcherf = None
         self._ohlcv_fetcherf = None
         self._mode = None
 
-        self.ticker_list = None
+        self.security_list = None
         self.name_list = None
         self.ohlcv_list = None
         self.compiled_list =None
@@ -60,79 +60,90 @@ class DataFetcher():
 
     def _set_stock_mode(self):
         self._mode = "stocks"
-        self._ticker_fetcherf = lambda date: stock.get_market_ticker_list(date, market="ALL")
+        self._security_fetcherf = lambda date: stock.get_market_ticker_list(date, market="ALL")
         self._name_fetcherf = stock.get_market_ticker_name
-        self._ohlcv_fetcherf = lambda ticker: stock.get_market_ohlcv("19700101","20301231", str(ticker))
+        self._ohlcv_fetcherf = lambda security: stock.get_market_ohlcv("19700101","20301231", str(security))
 
     def _set_etf_mode(self):
         self._mode = "ETFs"
-        self._ticker_fetcherf = stock.get_etf_ticker_list
+        self._security_fetcherf = stock.get_etf_ticker_list
         self._name_fetcherf = stock.get_etf_ticker_name
-        self._ohlcv_fetcherf = lambda ticker: stock.get_etf_ohlcv_by_date("19700101","20301231", str(ticker))
+        self._ohlcv_fetcherf = lambda security: stock.get_etf_ohlcv_by_date("19700101","20301231", str(security))
 
-    def _get_ticker_list(self, year_range = 0):
+    def _get_security_list(self, year_range = 0):
         """
-        @param range for value of 0, it will get currently available stocks. Other values go back in years from current year. This will introduce no longer available securities into the mix.
+        Gets a list of securities available within the specified year range.
+
+        Args:
+            year_range (int): The number of years to go back from the current year to fetch securities.
+                              For a value of 0, it fetches currently available stocks. Other values
+                              indicate how many years back from the current year to include securities
+                              that are no longer available.
+
+        Returns:
+            list: A list of securities merged from all years within the specified range.
         """
+        
         today = datetime.today()
-        date_list = [f"{ii}{today.month:02d}{today.day:02d}" for ii in range(today.year, today.year-1-year_range, -1)]
-        ticker_list = []
+        date_list = [f"{year}{today.month:02d}{today.day:02d}" for year in range(today.year, today.year-1-year_range, -1)]
+        security_list = []
         for date in date_list:
-            tickers = self._ticker_fetcherf(date)
-            if tickers:
-                ticker_list.append(tickers)
+            securities = self._security_fetcherf(date)
+            if securities:
+                security_list.append(securities)
         merge = lambda a, b: pd.merge(pd.DataFrame(a), pd.DataFrame(b), how="outer")
-        merged=pd.DataFrame(ticker_list[0])
-        for ticker in ticker_list[1:]:
-            merged = merge(merged, ticker)
+        merged=pd.DataFrame(security_list[0])
+        for security in security_list[1:]:
+            merged = merge(merged, security)
         return merged[0].values.tolist()
 
-    def _get_ticker_names(self, ticker_list):
+    def _get_security_names(self, security_list):
         name_list = []
-        for ticker in ticker_list:
-            name = self._name_fetcherf(ticker)
+        for security in security_list:
+            name = self._name_fetcherf(security)
             if not len(name):
-                name = f"{ticker} No Name"
+                name = f"{security} No Name"
             name_list.append(name)
         return name_list
 
-    def _get_ohlcvs_list_non_threaded(self, ticker_list, name_list):
+    def _get_ohlcvs_list_non_threaded(self, security_list, name_list):
         ohlcv_list=[]
-        list_size = len(ticker_list)
+        list_size = len(security_list)
         get_name = (lambda ii: name_list[ii]) if name_list else (lambda ii: "")
         TRIAL_CNT=0
-        for ii, ticker in enumerate(ticker_list):
+        for ii, security in enumerate(security_list):
             fetched = pd.DataFrame()
             trial_count=0
-            fetched = self._ohlcv_fetcherf(ticker)
+            fetched = self._ohlcv_fetcherf(security)
             while fetched.empty and trial_count<=TRIAL_CNT:
                 time.sleep(10)
                 try:
-                    fetched = self._ohlcv_fetcherf(ticker)
+                    fetched = self._ohlcv_fetcherf(security)
                 except Exception as e:
                     print(e)
                     fetched = pd.DataFrame()
                 trial_count += 1
             if trial_count > TRIAL_CNT:
-                print(f"\n{ticker} failed")
-            fetched = self._ohlcv_fetcherf(ticker)
+                print(f"\n{security} failed")
+            fetched = self._ohlcv_fetcherf(security)
             fetched = fetched.rename(columns=OHLCV_REPLACE_NAMINGS)
             fetched.index.name = "data"
-            print(f" {ii+1:5d}/{list_size} {ticker_list[ii]} {get_name(ii)} finished", end = "                       \r")
+            print(f" {ii+1:5d}/{list_size} {security_list[ii]} {get_name(ii)} finished", end = "                       \r")
             ohlcv_list.append(fetched)
         return ohlcv_list
 
-    def _get_ohlcvs_list_threaded(self, ticker_list, name_list):
+    def _get_ohlcvs_list_threaded(self, security_list, name_list):
         ohlcv_list=[]
         thread_list = []
-        list_size = len(ticker_list)
+        list_size = len(security_list)
         get_name = (lambda ii: name_list[ii]) if name_list else (lambda ii: "")
-        for ticker in ticker_list:
+        for security in security_list:
             new_thread = OhlcvThread({
-                                    "ticker": ticker,
+                                    "security": security,
                                     "fetcherf": self._ohlcv_fetcherf
                                     })
             thread_list.append(new_thread)
+        
         OUTPUT_LOCK.acquire()
         for ii, thread in enumerate(thread_list):
             thread.start()
@@ -143,30 +154,44 @@ class DataFetcher():
         for ii, thread in enumerate(thread_list):
             thread.join()
             OUTPUT_LOCK.acquire()
-            print(f" {ii+1:5d}/{list_size} {ticker_list[ii]} {thread.ticker} {get_name(ii)} finished", end = "                       \r")
+            print(f" {ii+1:5d}/{list_size} {security_list[ii]} {thread.security} {get_name(ii)} finished", end = "                       \r")
             OUTPUT_LOCK.release()
             ohlcv_list.append(thread.result)
         print("Data scraping complete.                        ")
         return ohlcv_list
 
-    def _get_ohlcvs_list(self, ticker_list, name_list, thread_mode=False):
-        ohlcv_list=[]
-        thread_list = []
-        list_size = len(ticker_list)
-        get_name = (lambda ii: name_list[ii]) if name_list else (lambda ii: "")
-        
+    def _get_ohlcvs_list(self, security_list, name_list, thread_mode=False):
+        """
+        Retrieves OHLCV (Open, High, Low, Close, Volume) data for a list of securities.
+
+        Args:
+            security_list (list): A list of securities for which OHLCV data needs to be retrieved.
+            name_list (list): A list of names corresponding to the securities in `security_list`.
+            thread_mode (bool, optional): Specifies whether to use threaded mode for data retrieval. 
+                                          Defaults to False.
+
+        Returns:
+            list: A list of OHLCV data for the given securities.
+
+        """
         if thread_mode:
-            return self._get_ohlcvs_list_threaded(ticker_list, name_list)
+            return self._get_ohlcvs_list_threaded(security_list, name_list)
         else:
-            return self._get_ohlcvs_list_non_threaded(ticker_list, name_list)
+            return self._get_ohlcvs_list_non_threaded(security_list, name_list)
 
     def get_all_data(self):
+        """
+        Retrieves all data for the specified mode.
+
+        Returns:
+            list: A list of dictionaries containing the symbol, name, and data for each security.
+        """
         print(f"Getting {self._mode}")
-        self.ticker_list = self._get_ticker_list()
-        self.name_list = self._get_ticker_names(self.ticker_list)
-        self.ohlcv_list = self._get_ohlcvs_list(self.ticker_list, self.name_list)    
-        list_size = len(self.ticker_list)
-        self.compiled_list = [{"symbol": self.ticker_list[ii],
+        self.security_list = self._get_security_list(0)
+        self.name_list = self._get_security_names(self.security_list)
+        self.ohlcv_list = self._get_ohlcvs_list(self.security_list, self.name_list)    
+        list_size = len(self.security_list)
+        self.compiled_list = [{"symbol": self.security_list[ii],
                                 "name": self.name_list[ii],
                                 "data": self.ohlcv_list[ii]}
                                 for ii in range(list_size) 
@@ -174,8 +199,13 @@ class DataFetcher():
         return self.compiled_list
     
     def save_all_data(self):
+        """Saves all the data obtained from get_all_data() method to a pickle file.
+
+        Returns:
+            list: The list of all data obtained.
+        """
         all_data = self.get_all_data()
-        with open(f"../data_pkl/KRX__copilot_test_{self._mode}_{datetime.now().__str__()[:10]}.pkl", "wb") as file:
+        with open(f"../data_pkl/KRX_{self._mode}_{datetime.now().__str__()[:10]}.pkl", "wb") as file:
             pickle.dump(all_data, file)
         return all_data
 
