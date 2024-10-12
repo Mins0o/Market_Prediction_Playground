bc51a33e43d76c2cec960b8ab7e864b4b671b4a0
Minsoo <memesoo.kang@gmail.com>
2024-10-12 18:56:59 +0900

    change rate parsing test

--- AssetOptimizationTool/ut/testcases/test_data.cpp
+++ AssetOptimizationTool/ut/testcases/test_data.cpp
@@ -1,10 +1,12 @@
 #include "data/data.h"  // testing class
+#include "_interfaces/data_interface.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "types.h"
 
 using ::asset_optimization_tool::AssetId;
 using ::asset_optimization_tool::ErrorCode;
+using ::asset_optimization_tool::modules::IAsset;
 using ::asset_optimization_tool::modules::Data;
 using ::testing::Return;
 
@@ -55,4 +57,24 @@ TEST(DataTest, GetAssetTableMethod) {
     asset_names.push_back(name);
   }
   ASSERT_THAT(asset_names, ::testing::UnorderedElementsAreArray(kAssetNames));
+}
+
+TEST(DataTest, GetAssetDataByIdsMethod) {
+  Data data;
+
+  std::set<AssetId> asset_ids = {1, 2, 3,4};
+  std::map<std::string, AssetId> asset_name_table;
+  std::vector<std::unique_ptr<IAsset>> asset_data;
+
+  ASSERT_EQ(data.LoadData(::kDataPath), ErrorCode::kSuccess);
+  ASSERT_EQ(data.GetAssetDataByIds(asset_ids, asset_data), ErrorCode::kSuccess);
+  ASSERT_EQ(asset_data.size(), 4);
+  for (const auto& asset : asset_data) {
+    ASSERT_THAT(kAssetNames, ::testing::Contains(asset->GetName()));
+    std::cout << asset->GetName() << "\t";
+    for (const auto& rate : asset->GetChangeRates()) {
+      std::cout << rate << "\t";
+    }
+    std::cout << std::endl;
+  }
 }
